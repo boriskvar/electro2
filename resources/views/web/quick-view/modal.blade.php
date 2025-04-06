df90812e34ee4be0ed111e77687cdbe74ecda3b6
Boris <bkvartyuk@gmail.com>
2025-04-06 14:23:22 +0300

    добавил стили в modal.blade.php

--- resources/views/web/quick-view/modal.blade.php
+++ resources/views/web/quick-view/modal.blade.php
@@ -1,59 +1,64 @@
 <div class="section">
-    <div class="container">
+    <!-- Карточка товара -->
+    <div class="row justify-content-center">
+        <div class="col-6 col-sm-4 col-md-3 text-center">
+            <div class="product-widget">
 
-
-        <!-- Заголовок -->
-        <div class="row">
-            <div class="col-md-12">
-                <div class="section-title text-center">
-                    <h3 class="title">Быстрый просмотр товара</h3>
+                <!-- Название и цена -->
+                <div class="product-body">
+                    <img src="{{ $product->images[0] ? asset('storage/img/' . $product->images[0]) : asset('storage/img/default.png') }}" alt="{{ $product->name }}" class="img-responsive center-block" style="max-width: 80px;">
+                    <h3 class="product-name">
+                        <a href="{{ route('products.show', $product->id) }}">{{ $product->name }}</a>
+                    </h3>
+                    <h4 class="product-price">
+                        @if ($product->old_price)
+                        <del class="product-old-price">${{ $product->old_price }}</del>
+                        @endif
+                        ${{ $product->price }}
+                    </h4>
                 </div>
             </div>
         </div>
-
-        <!-- Категория -->
-        <div class="row text-center mb-3">
-            <div class="col-md-12">
-                <h4 class="category-title mb-0">{{ $product->category->name }}</h4>
-            </div>
-        </div>
-
-        <!-- Карточка товара -->
-        <div class="row justify-content-center">
-            <div class="col-6 col-sm-4 col-md-3 text-center">
-                <div class="product-widget">
-                    <!-- Картинка -->
-                    <div class="product-img mb-2">
-                        <img src="{{ $product->images[0] ? asset('storage/img/' . $product->images[0]) : asset('storage/img/default.png') }}" alt="{{ $product->name }}" class="img-responsive center-block" style="max-width: 80px;">
-                    </div>
-
-                    <!-- Название и цена -->
-                    <div class="product-body">
-                        <h3 class="product-name">
-                            <a href="{{ route('products.show', $product->id) }}">{{ $product->name }}</a>
-                        </h3>
-                        <h4 class="product-price">
-                            @if ($product->old_price)
-                            <del class="product-old-price">${{ $product->old_price }}</del>
-                            @endif
-                            ${{ $product->price }}
-                        </h4>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- Характеристики -->
-        @foreach($attributes as $attributeName => $value)
-        <div class="row">
-            <div class="col-md-12 text-center">
-                <h5 class="attribute-header">{{ $attributeName }}</h5>
-            </div>
-            <div class="col-md-12 text-center">
-                <div class="value-cell">{{ $value }}</div>
-            </div>
-        </div>
-        @endforeach
-
     </div>
+
+    <!-- Характеристики -->
+    <div class="row justify-content-center">
+        <div class="col-6 col-sm-4 col-md-3">
+            <div class="text-center">
+                @foreach($attributes as $attributeName => $value)
+                <div class="attribute-item mb-2">
+                    <h5 class="attribute-header">{{ $attributeName }}</h5>
+                    <div class="value-cell">{{ $value }}</div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+
 </div>
+
+<style>
+    .product-widget .product-body {
+        padding-left: 0 !important;
+    }
+
+    /* Стиль для заголовков характеристик */
+    .attribute-header {
+        font-weight: 700;
+        line-height: 1;
+        background-color: #f5f5f5;
+        /* Цвет фона для заголовка */
+        color: #333;
+        /* Цвет текста */
+        padding: 12px 0;
+        /* Отступы сверху и снизу */
+        margin-top: 10px;
+        /* Отступ сверху от предыдущего элемента */
+        text-align: center;
+        /* Центрирование текста */
+        border-radius: 5px;
+        /* Скругленные углы */
+        box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
+        /* Легкая тень */
+    }
+</style>
